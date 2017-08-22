edb6c675b65f7fdc6031e3cad6618f6bc11043af
Filip Lav Maksimovic <gaming4ever93@gmail.com>
2017-08-22 14:23:19 +0200

    Update views.py

--- maybblog/views.py
+++ maybblog/views.py
@@ -1,20 +1,4 @@
-import datetime
+
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
-from django.template import  Template,loader
-def poyy(request):
-    todos_list = ["1","2","3"]
-    #dtemplate = loader.get_template('maybblog/todotemp.html')
-    #html = dtemplate.render({'todos_list' : todos_list})
-    #return HttpResponse(html)
-    return render(request,'todotemp.html',{'todos_list' : todos_list})
 
 
-def index(request, offset):
-    try:
-        offset = int(offset)
-    except ValueError:
-        raise Http404()
-    now = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "time in", offset, "will be", now
-    return HttpResponse(html)
